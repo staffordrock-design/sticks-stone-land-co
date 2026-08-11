f2a27dbf02ee16fdcd3c32f2d7b246ccc56ecafd
Kari Stafford <staffordrock@icloud.com>
2026-08-11 21:50:24 +0000

    External agent changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import ScrollToTop from './components/ScrollToTop';
 // Add page imports here
 import Home from './pages/Home';
 import ListingDetail from './pages/ListingDetail';
+import MineSiteDetail from './pages/MineSiteDetail';
 
 const AuthenticatedApp = () => {
   const { isLoadingAuth, isLoadingPublicSettings, authError, navigateToLogin } = useAuth();
@@ -39,6 +40,7 @@ const AuthenticatedApp = () => {
       {/* Add your page Route elements here */}
       <Route path="/" element={<Home />} />
       <Route path="/listings/:id" element={<ListingDetail />} />
+      <Route path="/mines/:id" element={<MineSiteDetail />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
