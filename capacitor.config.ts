4845589e890239f19cf7ee6f5eeef42b72cd089a
Kari Stafford <staffordrock@icloud.com>
2026-08-14 18:13:07 +0000

    External agent changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ssrockholdings.app',
+  appId: 'com.ssrockholdings.quarrymarketplace',
   appName: 'S&S Rock Holdings',
   webDir: 'dist'
 };
